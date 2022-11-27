d68f063fe27f7e2221955e5cdccb5ac441a0d949
vuongducphuong <47106573+vuongducphuong@users.noreply.github.com>
2022-11-27 19:54:47 +0700

    Thêm menu và report báo cáo tổng hợp doanh thu

--- addons_third/om_account_daily_reports/report/__init__.py
+++ addons_third/om_account_daily_reports/report/__init__.py
@@ -3,3 +3,4 @@
 from . import report_daybook
 from . import report_cashbook
 from . import report_bankbook
+from . import report_doanh_thu_tong_hop
--- addons_third/om_account_daily_reports/wizard/__init__.py
+++ addons_third/om_account_daily_reports/wizard/__init__.py
@@ -3,4 +3,4 @@
 from . import account_daybook_report
 from . import account_cashbook_report
 from . import account_bankbook_report
-
+from . import param_bao_cao_doanh_thu_tong_hop
