163c58dded05f33bf17d77eb61ccf40adfd0e742
vuongducphuong <vuongducphuong1992@gmail.com>
2022-12-06 23:44:19 +0700

    Thêm folder addons_erp và module ketoan

---
+++ addons_erp/ketoan/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Kế toán VN',
+    'version' : '1.0',
+    'summary': 'Kế toán VN',
+    'sequence': 10,
+    'description': """
+Kế toán VN
+    """,
+    'category': 'Accounting/Accounting',
+    'website': '',    
+    'depends' : ['account'],
+    'data': [
+        'views/ketoan_menu.xml',                
+    ],
+    'installable': True,
+    'application': True,
+    'license': '',
+}
