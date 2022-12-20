0af2078532526abee3cac4ec91e8f92ba9c2bab7
vuongducphuong <vuongducphuong1992@gmail.com>
2022-12-21 02:05:15 +0700

    checkin báo cáo sổ quỹ tiền mặt tạm thời, chưa sửa lại cách lấy

--- addons_erp/ketoan/__manifest__.py
+++ addons_erp/ketoan/__manifest__.py
@@ -17,7 +17,7 @@ Kế toán VN
         'views/ketoan_thu_chi_ngan_hang_chi_tien_view.xml',
         'views/ketoan_thu_chi_ngan_hang_thu_tien_view.xml',
         'views/ketoan_thu_chi_ngan_hang_chuyen_tien_noi_bo_view.xml',
-        'views/ketoan_bao_cao_so_cai.xml',
+        'views/ketoan_bao_cao_action.xml',
         
 
 
@@ -29,7 +29,9 @@ Kế toán VN
         'web.assets_backend': [
             'ketoan/static/src/css/report.css',            
             'ketoan/static/src/js/ketoan_bao_cao_so_cai.js',            
-            'ketoan/static/src/xml/ketoan_bao_cao_so_cai_view.xml',
+            'ketoan/static/src/js/ketoan_bao_cao_quy_tien_mat.js',            
+            'ketoan/static/src/xml/ketoan_bao_cao_so_cai_template.xml',
+            'ketoan/static/src/xml/ketoan_bao_cao_quy_tien_mat_template.xml',
         ],
     },    
     'installable': True,
--- addons_erp/ketoan/models/__init__.py
+++ addons_erp/ketoan/models/__init__.py
@@ -2,3 +2,4 @@
 
 from . import account_move
 from . import ketoan_bao_cao_so_cai
+from . import ketoan_bao_cao_quy_tien_mat
